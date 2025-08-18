2708ec03a3e3bd52e827f5ed78c590cfc4adb8af
Adhithyan <Adhithyan@DESKTOP-NIUOQBB>
2025-08-18 23:03:39 +0530

    Submit

--- C2TC_CoreJavaProgram/src/Daythree/encapsulation/EncapsulationDemo.java
+++ C2TC_CoreJavaProgram/src/Daythree/encapsulation/EncapsulationDemo.java
@@ -1,5 +1,16 @@
-package Daythree.encapsulation;
+//Program to demonstrate class and object 
+package com.tnsif.daythree.encapsulation;
 
 public class EncapsulationDemo {
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
 
 }
